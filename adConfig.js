c7d5dfc0dab4ddc4cd7b7bf8c5ab7c6798c19c21
Gerardo Herrera <gherrer1@binghamton.edu>
2018-02-10 21:58:24 -0500

    searching for tweets with keywords was easy enough. Swapped out twitter library btw

---
+++ adConfig.js
@@ -0,0 +1,6 @@
+const adConfig = {
+  searchQuery: '', // keywords you want to search tweets by
+  engagementMessage: '' // what you want to tweet to people who tweet tweets with above keywords
+};
+
+module.exports = adConfig;
--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-const Twitter = require('twitter-js-client').Twitter;
+const Twit = require('twit')
+const T = new Twit(require('./config'));
+
+const { searchQuery } = require('./adConfig');
+
+T.get('search/tweets', { q: searchQuery, count: 100 }, function(err, data, response) {
+  if(err) return console.log('err:', err);
+  const { statuses } = data;
+  let trimmedDownTweets = statuses.map(tweetData => ({
+    created_at: tweetData.created_at,
+    text: tweetData.text,
+    source: tweetData.source,
+    user: tweetData.user.screen_name,
+    truncated: tweetData.truncated
+  }));
+  console.log(trimmedDownTweets);
+});
