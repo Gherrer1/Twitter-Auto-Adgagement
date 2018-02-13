d610c09def3d514c45b6dc5854628935433ca647
Gerardo Herrera <gherrer1@binghamton.edu>
2018-02-13 03:50:54 -0500

    dawg I was certain i --assume-unchanged my config and adConfig, now I have consumer keys and api keys on github *sigh* time to generate new ones

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const config = {
   // I guess consumer keys identify your app
-  consumer_key: 'KdcB9CZgM0LyIjXOiJ5Mw7whO',
-  consumer_secret: 'kbSY35oeFEFQbNwelgjLJvs3ekVwU7aIOnVVqgk8M7reUL0ndC',
+  consumer_key: '',
+  consumer_secret: '',
   // while access tokens authorize the app to use your twitter account
   // better worded: allows app to make API requests on your own accounts behalf.
-  access_token: '944398105126100994-f2umWT8CLFsz6fcc03110lSLgb9yKoQ',
-  access_token_secret: 'oYzez39g5QE76y8dPNMUPgPeadMfrJqil1Yk74dNEwJFJ',
+  access_token: '',
+  access_token_secret: '',
   timeout_ms: 5000
 };
 
