974f4faddd14370bb7aef61757f1881eeff8653e
Gerardo Herrera <gherrer1@binghamton.edu>
2018-02-12 19:49:31 -0500

    stream not working the way i want - if i make the search query `cat OR dog`, i should be getting tweets with either of those keywords in them. I am not. Delve deeper into the docs

--- src/adConfig.js
+++ src/adConfig.js
@@ -1,6 +1,7 @@
 const adConfig = {
   searchQuery: 'lambo', // keywords you want to search tweets by
-  engagementMessage: '' // what you want to tweet to people who tweet tweets with above keywords
+  engagementMessage: '', // what you want to tweet to people who tweet tweets with above keywords
+  myTwitterHandle: ''
 };
 
 module.exports = adConfig;
--- src/tweetStream.js
+++ src/tweetStream.js
@@ -1,10 +1,38 @@
 const Twit = require('twit')
 const T = new Twit(require('./config'));
 const trimTweets = require('./trimTweets');
+const respondToTweet = require('./respondToTweet');
+const player = require('play-sound')();
 
-var stream = T.stream('statuses/filter', { track: 'javascript' });
+const { searchQuery, myTwitterHandle } = require('./adConfig');
+
+var stream = T.stream('statuses/filter', { track: searchQuery });
+
+function isRetweet(tweet) {
+	return tweet.text.startsWith('RT');
+}
+
+function isFromMe(tweet) {
+	return tweet.user.toLowerCase() === myTwitterHandle;
+}
 
 stream.on('tweet', function(tweet) {
-  const trimmedTweet = trimTweets([tweet]);
-  console.log(trimmedTweet);
+  const [trimmedTweet] = trimTweets([tweet]);
+  if(isRetweet(trimmedTweet)) {
+  console.log('Retweet:', trimmedTweet);
+		return player.play('./sounds/hihat.wav', (err) => console.log(err ? err : ''));
+  }
+	if(isFromMe(trimmedTweet)) {
+		console.log('From Me:', trimmedTweet);
+		return;
+	}
+	console.log('OG Tweet:', trimmedTweet);
+	player.play('./sounds/openhat.wav', err => console.log(err ? err : ''));
+	// reply
+	// const user = trimmedTweet.user;
+	// const tweetID = trimmedTweet.id_str;
+	// const message = `@${user} javascript is awesome`;
+	// respondToTweet(tweetID, message, T)
+	// 	.then(tweetResponse => console.log('Response:', trimTweets([tweetResponse])))
+	// 	.catch(err => console.log(err));
 });
